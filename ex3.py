01d08b59a0d85dbf8d0d2f4d7c1bd606f37b344e
Tarevler <172126517@qq.com>
2022-10-16 23:14:07 +0800

    first commit

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# print
+print ("I will now count my chickens:")
+# 
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now Iwill count the eggs:")
+
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+
+print ("Is it true that 3 + 2 < 5 - 7 ?")
+# 判断3+2 是否less-than 5-7
+print (3 + 2 < 5 -7)
+# 计算 3+2
+print ("What is 3 + 2 ?", 3 + 2)
+# 计算 5-7
+print ("What is 5 - 7 ?", 5 - 7)
+
+print ("Oh,that's why it's False.")
+
+print ("How about some more.")
+
+print ("Is it greater?",5 > -2)
+print ("is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
+
